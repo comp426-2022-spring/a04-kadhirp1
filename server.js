71283f4863ca9a2a2dc0a15392c1002846ad758a
Kadhir Patchamuthu <kadhirp@ad.unc.edu>
2022-04-22 17:52:44 -0400

    Added the database logs and /app/log/access thought it is not conditionally true on debug == true yet

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 const express = require('express')
 const app = express()
 const args = require("minimist")(process.argv.slice(2))
+const db = require("./database.js")
 //Command Line Argument for port here
 args["port"]
+args["help"]
+args["log"]
+args["debug"]
+args["h"]
+
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
+if (args.help || args.h) {
+  console.log(help)
+  process.exit(0)
+}
+
 const port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
+    console.log(`Server is cumming and running on ${port}`)
     //Same asconsole.log('App is running on port %PORT%'.replace('$PORT$', port))
 })
 
+app.use((req, res, next) => {
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?)')
+  const info = stmt.run(req.ip, req.user, Date.now(), req.method, req.url, req.protocol, req.httpVersion, res.statusCode, req.header['referer'], req.headers['user-agent'])
+  
+  next()
+})
 
 function coinFlip() {
     return (Math.floor(Math.random() * 2) == 0 ) ? 'heads' : 'tails';
@@ -60,20 +94,28 @@ function coinFlip() {
 
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
-    res.type("text/json")
+})
+
+app.get('/app/log/access', (req, res) => {
+  try {
+    const stmt = db.prepare('SELECT * FROM accessLog').all()
+    res.status(200).json(stmt)
+} catch (e){
+    console.error(e)
+}
 })
 
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
     res.status(200).json({'flip': flip})
-    res.type("text/json")
+    
 })
 
 app.get('/app/flips/', (req, res) => {
     var flips = coinFlips(1)
     var count = countFlips(flips)
     res.status(200).json( {'raw': flips, 'summary':count})
-    res.type("text/json")
+    
 })
 
 app.get('/app/flips/:number', (req, res) => {
@@ -88,7 +130,7 @@ app.get('/app/flip/call/:guess', (req, res) => {
     
     var result = flip == req.params.guess ? 'win':'lose'
     res.status(200).json({'call': req.params.guess, 'flip': flip, 'result':result })
-    res.type("text/json")
+    
     
     
 })
@@ -96,7 +138,7 @@ app.get('/app/flip/call/:guess', (req, res) => {
 
 app.use(function(req, res) {
     res.status(404).send("404 NOT FOUND")
-    res.type("text/plain")
+    
 }
 
 )
